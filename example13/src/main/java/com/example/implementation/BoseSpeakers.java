28a9321e83614db0e72984d2234df32bdeb46751
Daniel Molina <daniel.molina@rnpn.gob.sv>
2025-07-19 12:03:05 -0600

    :pushpin: 13

---
+++ example13/src/main/java/com/example/implementation/BoseSpeakers.java
@@ -0,0 +1,11 @@
+package com.example.implementation;
+
+import com.example.interfaces.Speakers;
+
+public class BoseSpeakers implements Speakers {
+
+    @Override
+    public String makeSound() {
+        return "Bose Speaker is playing music";
+    }
+}
---
+++ example13/src/main/java/com/example/implementation/SonySpeaker.java
@@ -0,0 +1,9 @@
+package com.example.implementation;
+
+public class SonySpeaker implements com.example.interfaces.Speakers {
+
+    @Override
+    public String makeSound() {
+        return "Sony Speaker is playing music";
+    }
+}
